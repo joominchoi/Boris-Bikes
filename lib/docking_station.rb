0632020a8b370887a42705d26a161a987b401611
JooMin Choi <joominchoi23@gmail.com>
2021-05-27 19:12:39 +0100

    Challenge 11 - updated to make docking a bike and checking a bike is docked possible

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,22 @@
 require_relative "bike"
 
+# in docking_station.rb
 class DockingStation
-
+  # this will essentially write the method on any instances of this class
+  # # def bike
+  # #   @bike
+  # # end
+  attr_reader :bike
+  
   def release_bike
     Bike.new
   end
 
   def dock_bike(bike)
+    # we need to return the bike we dock
+    @bike = bike
   end
+
+  
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,4 @@
-require "docking_station.rb"
+require "docking_station"
 
 # describe DockingStation do
 #   it "DockingStation responds to release_bike" do
@@ -15,11 +15,24 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
-  it {is_expected.to respond_to :dock_bike.with(1).argument}
-
+  it {is_expected.to respond_to(:dock_bike).with(1).argument}
+  # first, let's update our test for #dock
   it "docks working bikes" do
     # dock = subject.dock_bike
     # expect(dock).to be_working
+    bike = Bike.new
+    # we want to return the bike we dock
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it {is_expected.to respond_to :bike}
+
+  # let's update our test for #bike
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    # Again, we need to return the bike we just docked
+    expect(subject.bike).to eq bike
   end
 
 end
