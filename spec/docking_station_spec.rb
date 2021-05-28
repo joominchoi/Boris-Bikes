ff2864c6c077753193577c81c2bd8b28a256cd89
JooMin Choi <joominchoi23@gmail.com>
2021-05-28 15:56:07 +0100

    Challenge 12 - raising exceptions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,37 +1,32 @@
 require "docking_station"
 
-# describe DockingStation do
-#   it "DockingStation responds to release_bike" do
-#     expect(DockingStation).to respond_to :release_bike 
-#   end
-# end
-
-# one-liner syntax
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
 
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe "#release_bike" do
+    it "raises an error when there are no bikes available" do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
   end
+    
+    it "release a bike" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
 
   it {is_expected.to respond_to(:dock_bike).with(1).argument}
-  # first, let's update our test for #dock
+  
   it "docks working bikes" do
-    # dock = subject.dock_bike
-    # expect(dock).to be_working
     bike = Bike.new
-    # we want to return the bike we dock
     expect(subject.dock_bike(bike)).to eq bike
   end
 
   it {is_expected.to respond_to :bike}
 
-  # let's update our test for #bike
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock_bike(bike)
-    # Again, we need to return the bike we just docked
     expect(subject.bike).to eq bike
   end
 
